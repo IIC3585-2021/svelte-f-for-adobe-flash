c6853930862ef457f958708a318870cdd1b0e975
frvalenzuela <frvalenzuela@uc.cl>
2021-06-22 16:50:32 -0400

    first-big-div

---
+++ f-for-free-games/src/components/BigDiv.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { onMount } from 'svelte';
+	import { games, fetchGames } from './store.js'
+	import GameDiv from "./components/GameDiv.svelte"
+
+	onMount(fetchGames)
+</script>
+<main>
+	{#each $games as game}
+		<GameDiv game= {game}/>
+	{/each}
+</main>
+
+<style>
+</style>
