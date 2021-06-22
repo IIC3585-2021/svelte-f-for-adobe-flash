cd44201094b8bc5fafdbb81e05be150b0a7aa255
Okhan97 <iapenafiel@uc.cl>
2021-06-22 15:49:53 -0400

    fetch from API

--- f-for-free-games/src/App.svelte
+++ f-for-free-games/src/App.svelte
@@ -1,11 +1,35 @@
 <script>
 	export let name;
+	let games;
+	const options = {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-key": "24b838a613msh0a610b53a6fb83ep1fe6b6jsn2fda2eb88375",
+			"x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
+			"useQueryString": true
+		}
+    }
+	const fetchGames = (async () => {
+		const req = await fetch("https://free-to-play-games-database.p.rapidapi.com/api/games", options)
+		const aux = await req.json()
+		console.log(aux)
+		return aux
+	})()
+
+
+	
 </script>
 
+{#await fetchGames}
+<p>LOADING...</p>
+{:then data}
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
+{:catch error}
+<p>An error occurred!</p>
+{/await}
 
 <style>
 	main {
